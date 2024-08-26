e9028a703b2addf536f0da10a95e163192e7978d
Sibghat776 <ullahsibghat786@gmail.com>
2024-08-26 04:19:35 -0700

    In functions

--- Functions/app.js
+++ Functions/app.js
@@ -1,16 +1,26 @@
 //Functions
 
-function barBarHo () {
-    console.log(`Assalam u alaikum`)
-    console.log(`Bhai Kesa ha`)
-    console.log(`Kia haal ha`)
-    console.log(`Ajj kal kia kaam chal raha ha`)
-} 
+// function barBarHo () {
+//     console.log(`Assalam u alaikum`)
+//     console.log(`Bhai Kesa ha`)
+//     console.log(`Kia haal ha`)
+//     console.log(`Ajj kal kia kaam chal raha ha`)
+// } 
 
-barBarHo();
-barBarHo();
-barBarHo();
-barBarHo();
-barBarHo();
-barBarHo();
-barBarHo();
+// barBarHo();
+// barBarHo();
+// barBarHo();
+// barBarHo();
+// barBarHo();
+// barBarHo();
+// barBarHo();
+
+function juice (fruit) {
+    console.log(`${fruit} ko refrigirator se nikalo`)
+    console.log(`Phir is ka chilka utaro`)
+    console.log(`Phir ${fruit} k slices banao`)
+    console.log(`Juicer machine me daaldo`)
+    console.log(`Ab us me cheeni aur dudh daalo`)
+    console.log(`Ab ${fruit} shake ko glass me nikal ka pee jao`)
+}
+juice (`Haathi`)
