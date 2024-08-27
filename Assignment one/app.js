94436db3a593dca6c2164197137044e6f88d7bca
Sibghat776 <ullahsibghat786@gmail.com>
2024-08-27 06:31:15 -0700

    Book Assignment one done

---
+++ Assignment one/app.js
@@ -0,0 +1,11 @@
+//Alerts
+
+alert(`Error! Please Enter a Valid Password`)
+
+alert(`Welcome to JS Land...\nHappy Coding!`)  //Line Breaking by \n
+
+alert("Welcome to JS Land..")
+
+alert("Happy Coding!\nPreventing this page fromcreating additional dialoges.")
+
+alert("Hello... I can run js throughmy web browser's console")
--- Functions/app.js
+++ Functions/app.js
@@ -15,12 +15,32 @@
 // barBarHo();
 // barBarHo();
 
-function juice (fruit) {
-    console.log(`${fruit} ko refrigirator se nikalo`)
-    console.log(`Phir is ka chilka utaro`)
-    console.log(`Phir ${fruit} k slices banao`)
-    console.log(`Juicer machine me daaldo`)
-    console.log(`Ab us me cheeni aur dudh daalo`)
-    console.log(`Ab ${fruit} shake ko glass me nikal ka pee jao`)
+// function juice (fruit) {
+//     console.log(`${fruit} ko refrigirator se nikalo`)
+//     console.log(`Phir is ka chilka utaro`)
+//     console.log(`Phir ${fruit} k slices banao`)
+//     console.log(`Juicer machine me daaldo`)
+//     console.log(`Ab us me cheeni aur dudh daalo`)
+//     console.log(`Ab ${fruit} shake ko glass me nikal ka pee jao`)
+// }
+// juice (`Haathi`)
+
+
+
+// function addNum (num1 , num2 , num3) //Parameters
+// {
+//     return num1 + num2 - num3
+//     console.log(`Sibghat Ullah Aik bohot acha bacha ha`)
+// }
+
+// const naam = addNum (24 , 45 , 1777)
+
+// console.log(naam)
+
+var haji = function haji () {
+    console.log(`haji`)
 }
-juice (`Haathi`)
+
+console.log(haji)
+
+haji ()
