8c866923cf106307710ff4609dd5551f65e145c3
Sibghat776 <ullahsibghat786@gmail.com>
2024-08-29 12:44:38 +0500

    Book Assignment Three Done...

---
+++ Assignment three/app.js
@@ -0,0 +1,19 @@
+//Variable for Numbers
+
+var age = 16
+alert("I am" + " "  + age + " " + "Years Old.")
+
+var visit = 15
+alert(`You have Visited this site ${visit} times...`)
+
+var birthYear = 2008   //Data type of my declared variable is number
+document.write(`My birth Year is ${birthYear}\n`)
+document.write(`Data type of my declared variable is number\n`)
+
+
+var buyersName = `Jhon Doe\n`
+var storeName = `www.shukarclothing.com\n`
+var quantity = `6 T-Shirts\n`
+document.write(`${buyersName} ordered ${quantity} on ${storeName} Clothing Store.`)
+
+//END
