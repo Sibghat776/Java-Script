290da88c33fa5324af04cf6f9cfed4828e859cd1
Sibghat776 <ullahsibghat786@gmail.com>
2024-08-30 01:13:27 -0700

    Second assignment of book

---
+++ Assignment two/app.js
@@ -0,0 +1,52 @@
+// Variable --> var  for String --> ""
+
+var username;
+var myName;
+username = "Sibghat Ullah"
+myName = username
+
+var dua;  // Declaration
+dua = "Assalam u alaikum"  // "" <-- strings
+alert(dua)
+
+var studentName = "Sibghat Ullah"
+var studentAge = "16 years old"
+var studentQualification = "Ninth Passed..."
+
+alert(studentName)
+alert(studentAge)
+alert(studentQualification)
+
+var khana = "Biryani"
+alert(khana)
+
+khana = "Biryan"
+alert(khana)
+
+khana = "Birya"
+alert(khana)
+
+khana = "Biry"
+alert(khana)
+
+khana = "Bir"
+alert(khana)
+
+khana = "Bi"
+alert(khana)
+
+khana = "B"
+alert(khana)
+
+var email = "ullahsibghat786@gmail.com"
+alert("My email address is " + email)  //Concatenation
+
+var book = "A smarter way to learn javascript"
+alert("I am Trying to learn from the Book " + book)
+
+document.write("Yah! I can Write HTML Content Through JavaScript")
+
+var design = `"==========;;'+';;=========="`
+alert(design)
+
+document.write(design)
