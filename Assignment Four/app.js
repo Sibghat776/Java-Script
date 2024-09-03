927d77c1e2dc82575c070c5783cb46019c73035e
Sibghat776 <ullahsibghat786@gmail.com>
2024-09-03 07:20:54 -0700

    Books Assignment four complete

---
+++ Assignment Four/app.js
@@ -0,0 +1,45 @@
+// Variables Names : Legal or Illegal
+
+var number1 = 15 , number2 = 30 , number3 = 45 
+//Declaration of three variables in one Statement
+
+
+//Declarattion of Legal Variables
+var heading = "<h1> Rules for Naming Js Variables </h1>" 
+document.write(heading)
+
+var $Rule_1 = "Variable names can only contain numbers, $ and _. For Example : $my_1stVariable"
+document.write("<p>" + $Rule_1 + "</p>")
+
+var $Rule_2 = "Variables must begin with a letter , $ or _. For Example : $name , _name or name"
+document.write("<p>" + $Rule_2 + "</p>")
+
+var $Rule_3 = "Variables names are case sensitive"
+document.write("<p>" + $Rule_3 + "</p>")
+
+var $Rule_4 = "Variables should not be JS Keyword"
+document.write("<p>" + $Rule_4 + "</p>")
+
+var $Rule_5
+//Declaration of Five (5) Legal Variables Complete...
+
+
+//Declaration of Five (5) Illegal Variables
+
+var var = 40  
+// Variables should not be JS Keyword
+
+var 1myvariable =  
+//Not start with anumber
+
+var my variable =  
+//Space
+
+var myVariable! =  
+//Special character
+
+var function =  
+// JavaScript keyword !!!
+
+
+
