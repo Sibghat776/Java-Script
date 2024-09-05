4422895df58fc6f4c3f72466b1c0665691b1f3ae
Sibghat776 <ullahsibghat786@gmail.com>
2024-09-05 10:23:09 +0500

    Book Assignment 5 to 9 on Progress

---
+++ Assignment Six-Nine/app.js
@@ -0,0 +1,77 @@
+
+// Math Expressions
+
+// Task One (1)
+var a = 10 , b = 12
+
+
+document.write("<p>The Result:</p>")
+document.write("The value of a is : " + a)
+
+
+document.write("<p>,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,</p>")
+
+
+var a = ++a  //Pre-Increment
+document.write("The value of ++a is : " + a)
+document.write("</br>")
+document.write("Now The value of a is : " + a)
+
+
+document.write("</br>")
+document.write("</br>")
+document.write("</br>")
+
+
+a = a++  //Post-Increment
+document.write("The value of a++ is : " + a)
+document.write("</br>")
+a + a++
+document.write("Now The Value of a is : " + a)
+
+
+document.write("</br>")
+document.write("</br>")
+document.write("</br>")
+
+
+a = --a
+document.write("The Value of --a is : " + a)
+document.write("</br>")
+document.write("Now the value of a is : " + a)
+
+
+document.write("</br>")
+document.write("</br>")
+document.write("</br>")
+
+
+a = a--
+document.write("The Value of a-- is : " + a)
+document.write("</br>")
+a + a--
+document.write("Now The Value of a is : " + a)
+
+
+// Task Two (2)
+// In Console
+var a = 2 , b = 1;
+--a;
+console.log(a)
+
+c = --a - --b;
+console.log(c)
+
+d = --a - --b + ++b;
+console.log(d)
+
+e = --a - --b + ++b + b--;
+console.log(e)
+
+
+// Task Three (3)
+var userName = prompt("Sir ! What's Your Name ??")
+alert("Assalam u alaikum Mr ! " + userName)
+
+// Task Five (5)
+var 
