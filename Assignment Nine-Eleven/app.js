b847176fde273f28aa81e15591c1ef8d5845bbff
Sibghat776 <ullahsibghat786@gmail.com>
2024-09-10 09:27:49 +0500

    On Progress

--- Assignment Nine-Eleven/app.js
+++ Assignment Nine-Eleven/app.js
@@ -1,5 +1,28 @@
 // User Input and Conditional Statement
+
+// Task # 01
 var userInputCity = prompt(`Enter the biggest city of Pakistan!`);
+if(userInputCity == `Karachi`){
+    alert(`Welcome to City of Lights✔`)
+}
 
+// Task # 02
+var userInputGender = prompt(`What is your Gender??`)
+if(userInputGender == `Male`){
+    alert(`Assalam u alaikum Bhai Jaan`)
+}else{
+    alert(`Assalam u alaikum Ma'am`)
+}
 
+// Task # 03
+var userInputSignal = prompt(`Which Signal is in your traffic road`)
+if(userInputSignal == `Red`){
+    alert(`Must Stop`)
+}if(userInputSignal == `Green`){
+    alert(`Move On`)
+}if(userInputSignal == `Yellow`){
+    alert(`Ready To Move`)
+}
+
+ 
 
