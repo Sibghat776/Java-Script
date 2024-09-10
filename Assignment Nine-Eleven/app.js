ff23ef07375b2bf53f1bb58a2e54696afe07f84b
Sibghat776 <ullahsibghat786@gmail.com>
2024-09-10 11:40:25 +0500

    On Last Task

--- Assignment Nine-Eleven/app.js
+++ Assignment Nine-Eleven/app.js
@@ -1,28 +1,72 @@
 // User Input and Conditional Statement
 
-// Task # 01
-var userInputCity = prompt(`Enter the biggest city of Pakistan!`);
-if(userInputCity == `Karachi`){
-    alert(`Welcome to City of Lights✔`)
-}
+// // Task # 01
+// var userInputCity = prompt(`Enter the biggest city of Pakistan!`);
+// if(userInputCity == `Karachi`){
+//     alert(`Welcome to City of Lights✔`)
+// }
 
-// Task # 02
-var userInputGender = prompt(`What is your Gender??`)
-if(userInputGender == `Male`){
-    alert(`Assalam u alaikum Bhai Jaan`)
-}else{
-    alert(`Assalam u alaikum Ma'am`)
-}
+// // Task # 02
+// var userInputGender = prompt(`What is your Gender??`)
+// if(userInputGender == `Male`){
+//     alert(`Assalam u alaikum Bhai Jaan`)
+// }else{
+//     alert(`Assalam u alaikum Ma'am`)
+// }
 
-// Task # 03
-var userInputSignal = prompt(`Which Signal is in your traffic road`)
-if(userInputSignal == `Red`){
-    alert(`Must Stop`)
-}if(userInputSignal == `Green`){
-    alert(`Move On`)
-}if(userInputSignal == `Yellow`){
-    alert(`Ready To Move`)
-}
+// // Task # 03
+// var userInputSignal = prompt(`Which Signal is in your traffic road`)
+// if(userInputSignal == `Red`){
+//     alert(`Must Stop`)
+// }if(userInputSignal == `Green`){
+//     alert(`Move On`)
+// }if(userInputSignal == `Yellow`){
+//     alert(`Ready To Move`)
+// }
 
- 
+// // Task # 04
+// var userInputFuel = +prompt(`How much is the fuel for your car??`) 
+// if(userInputFuel <= 0.25){
+//     alert(`Please Refill the fuel in this Car`)
+// }
+
+// // Task # 05
+// var a = 4;
+// if(++a === 5){
+//     alert(`given condition for variable a is true`);
+// }
+// console.log(a)
+
+// // Task # 06 
+// var b = 82;
+// if(b++ === 83){
+//     alert(`given condition for var b is true`)
+// }
+// console.log(b)
+
+// var c = 12;
+// if(c++ === 13){
+//     alert(`Condition 1 is true`)
+// }if(c === 13){
+//     alert(`Condition 2 is true`)
+// }if(++c < 14){
+//     alert(`Condition 3 is true`)
+// }if(c === 14){
+//     alert(`Condition 4 is true`)
+// }
+// console.log(c)
+
+// var materialCost = 350, labourCost = 150;
+// var totalCost = materialCost + labourCost
+// if(totalCost == labourCost + materialCost){
+//     alert(`The Cost Equals`)
+// }
+
+// if(true){
+//     alert(`True`);
+// }
+
+// if(`Car` < `Cat`){
+//     alert(`Car is smaller than Cat`)
+// }
 
