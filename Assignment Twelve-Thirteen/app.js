b7a8eb4a90891e192f90ab40131fc7dc7f1ee3ee
Sibghat776 <ullahsibghat786@gmail.com>
2024-09-16 10:47:04 +0500

    Book Assignment 12-13 Started

---
+++ Assignment Twelve-Thirteen/app.js
@@ -0,0 +1,26 @@
+// If...Else / Else if Statements
+// Testing set of conditions
+
+// // Task # 01
+// // Take input from user using prompt
+// let inputChar = prompt("Enter a character:");
+
+// // Get the ASCII code of the character
+// let asciiCode = inputChar.charCodeAt(0);
+
+// // Check if it's a number (0-9)
+// if (asciiCode >= 48 && asciiCode <= 57) {
+//     console.log(inputChar + " is a number.");
+// }
+// // Check if it's an uppercase letter (A-Z)
+// else if (asciiCode >= 65 && asciiCode <= 90) {
+//     console.log(inputChar + " is an uppercase letter.");
+// }
+// // Check if it's a lowercase letter (a-z)
+// else if (asciiCode >= 97 && asciiCode <= 122) {
+//     console.log(inputChar + " is a lowercase letter.");
+// }
+// // If it's neither, handle it as another character
+// else {
+//     console.log(inputChar + " is not a number or letter.");
+// }
