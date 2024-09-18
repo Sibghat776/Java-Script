9e63f901cb40bab526e3f926ca73a1007448fba0
Sibghat776 <ullahsibghat786@gmail.com>
2024-09-18 11:26:00 +0500

    Till Task no 08 Completed ....

---
+++ Assignment Forteen-Sixteen/app.js
@@ -0,0 +1,50 @@
+// Arrays
+
+
+// // Task 01
+// var studentsNames = []
+
+
+// // Task 02 
+// var names = { array: []}
+
+
+// // Task 03
+// var stringsArray = ["Sibghat" , "Ullah"]
+
+
+// // Task 04
+// var numbersArray = [1, 2, 3]
+
+
+// // Task 05
+// var booleanArray = [true , false]
+
+
+// // Task 06
+// var mixedArray = ["Sibghat Ullah" , 70 , true]
+
+
+// // Task 07 
+// var availableEducation = ["SSC" , "HSC" , "BCS" , "BS" , "B.Com" , "MS" , "M.Phil." , "PhD"]
+// console.log(availableEducation)
+
+
+// // Task 08
+// var students3 = ["Michael", "John", "Tony"]
+// var students_Score = [320, 230, 480]
+// var total = 500
+
+// document.write("Score of " + students3[0] + " is " + students_Score[0] + " .Percentage " + students_Score[0] * 100 / total + "%" + "</br>")
+// document.write("Score of " + students3[1] + " is " + students_Score[1] + " .Percentage " + students_Score[1] * 100 / total + "%" + "</br>")
+// document.write("Score of " + students3[2] + " is " + students_Score[2] + " .Percentage " + students_Score[2] * 100 / total + "%" + "</br>")
+
+
+// Task 09
+
+
+
+
+
+
+
