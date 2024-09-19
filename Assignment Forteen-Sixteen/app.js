ba4460d384f0adaca3775a508cf1082090741bff
Sibghat776 <ullahsibghat786@gmail.com>
2024-09-19 12:36:29 +0500

    Task no 10 in progress

--- Assignment Forteen-Sixteen/app.js
+++ Assignment Forteen-Sixteen/app.js
@@ -40,7 +40,43 @@
 // document.write("Score of " + students3[2] + " is " + students_Score[2] + " .Percentage " + students_Score[2] * 100 / total + "%" + "</br>")
 
 
-// Task 09
+// // Task 09
+// var colorArray = ["Red" , "Blue" , "Green"]
+
+// console.log(colorArray)
+
+// console.log(colorArray.unshift("Yellow")) // Adding Element in start
+// console.log(colorArray.push("Black"))
+// console.log(colorArray.unshift("White" , "Gray"))
+// console.log(colorArray.shift())
+// console.log(colorArray.pop())
+
+// console.log(colorArray)
+
+// var userInput = prompt("App kon se number pe Brown color add karna chahte ho")
+// console.log(colorArray.splice(userInput, 1, "Brown"))
+// console.log(colorArray)
+
+// var userInput = prompt("App kon se number pe color delete karna chahte ho")
+// console.log(colorArray.splice(1, userInput))
+// console.log(colorArray)
+
+
+// // Task 10
+// var sortNumbers = [320 , 230 , 480 , 120];
+// document.write("Score of Students : " + sortNumbers + "</br>");
+
+// sortNumbers.sort((a, b) => a - b);  //.sort () -->> for ascending order(Small to Big)
+// document.write("Orderd Score of Students : " + sortNumbers);
+
+
+// Task 11
+
+
+
+
+
+
 
 
 
