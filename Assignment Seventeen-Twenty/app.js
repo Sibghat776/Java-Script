7594fa220bdab70cc5bfdf1ebdb36294065e584b
Sibghat776 <ullahsibghat786@gmail.com>
2024-09-24 11:15:10 +0500

    Task no 04 done

--- Assignment Seventeen-Twenty/app.js
+++ Assignment Seventeen-Twenty/app.js
@@ -1,4 +1,49 @@
-// Arrays And Loop
- let matrixNumber = [0, 1, 2, 3,[1, 0, 1, 2], 2, 1, 0, 1]
+// // Arrays And Loop
 
- document.write(matrixNumber)
+
+
+// // Task 01
+// let mutidimensionalArray = [[]]
+
+
+// // Task 02
+// let matrixNumber = [
+//     [0, 1, 2, 3].join(" ") + "</br>",
+//     [1, 0, 1, 2].join(" ")  + "</br>",
+//     [2, 1, 0, 1].join(" ") 
+// ]
+// document.write(matrixNumber.join(" "))
+
+
+
+// // Task 03
+// for (i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+
+
+// // Task 04
+// var userTableInput = +prompt("Enter a Number to show its Multiplication table...")
+// for (i = 1; i <= 10; i++) {
+//     console.log(userTableInput*i)
+// }
+ 
+
+
+// // Task 05
+// var userTableInput = +prompt("Enter length Multiplication table...")
+// for (i = 1; i <= userTableInput; i++) {
+//     console.log(2*i)
+// }
+
+
+
+// Task 06
+let 
+
+
+
+
+
+
