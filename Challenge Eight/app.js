5c53f7e24a09effa5a8d345fb657e16b0e198bfd
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-04 08:54:23 -0700

    Slides Challenge Eight Completed

--- Challenge Eight/app.js
+++ Challenge Eight/app.js
@@ -1,1 +1,11 @@
 //Function Challenge
+function fullName(firstName, lastName) {
+    return firstName + lastName;
+}
+
+let result = fullName("Sibghat ", "Ullah").toUpperCase()
+console.log(result)
+
+
+// Refactor in Object Parameters
+console.log(fullName({firstName : "Sibghat ", lastName : "Ullah"}))
