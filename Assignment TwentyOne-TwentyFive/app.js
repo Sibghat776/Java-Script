b650458eea2174dded2227c330346ae8670bf049
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-07 07:15:20 -0700

    Book Assignment 21-25 Started

---
+++ Assignment TwentyOne-TwentyFive/app.js
@@ -0,0 +1,20 @@
+// Strings Method
+
+// // Task # 01
+// let firstName = prompt(`What is your first Name ???`);
+// let lastName = prompt(`What is your Last Name ???`);
+
+// let fullName = firstName + ` ` + lastName;
+// alert(`Asslam u alaikum Sir ! ` + fullName);
+
+
+
+// // Task # 02
+// let userMobile = prompt(`What your Mobiles Name???`);
+// document.write(`User's Mobile : ${userMobile} ;`);
+
+// document.write(`${"</br>"} Length of Strings :  ${userMobile.length} ;`);
+
+
+
+// Task # 03
