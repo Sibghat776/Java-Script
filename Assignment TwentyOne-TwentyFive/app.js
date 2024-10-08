46fd9f378b1930e363a7ba9cf6e2d5b7e9fa758f
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-08 08:51:13 +0500

    Task number 06 is started

--- Assignment TwentyOne-TwentyFive/app.js
+++ Assignment TwentyOne-TwentyFive/app.js
@@ -9,6 +9,7 @@
 
 
 
+
 // // Task # 02
 // let userMobile = prompt(`What your Mobiles Name???`);
 // document.write(`User's Mobile : ${userMobile} ;`);
@@ -17,4 +18,37 @@
 
 
 
-// Task # 03
+
+// // Task # 03
+// document.write("Name of our country : ");
+// var ourCountry = "Pakistan";
+// var indexNo = `Index Number of "n" : ${ourCountry.indexOf("n")}`;
+// document.write(ourCountry + "</br>");
+// document.write(indexNo);
+
+
+
+
+// // Task # 04
+// document.write("Assalam u alaikum" + "</br>");
+// var checkLastA = "Assalam u alaikum".lastIndexOf("a");
+// document.write(`Index of last a : ${checkLastA}`);
+
+
+
+
+// // Task # 05
+// let ourCountry = "Pakistan </br> ";
+// let indexNo03 = `Character at index no 04 is : ${ourCountry.charAt(4)}`;
+// document.write(ourCountry);
+// document.write(indexNo03);
+
+
+
+
+// Task # 06
+
+
+
+
+
---
+++ For Muneeba/app.js
@@ -0,0 +1,3 @@
+var forMuneebaQue = prompt("Kia khaegi??")
+
+forMuneebaQue == "kuch bhi nahi" ? alert("Chhal thik ha behen"): alert(forMuneebaQue + " la raha hun") 
