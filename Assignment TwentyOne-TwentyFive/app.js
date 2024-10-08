9f4ab09096d20f6d186ad7a5eb8d5afc7b45461f
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-08 05:38:44 -0700

    Task no 12 is done

--- Assignment TwentyOne-TwentyFive/app.js
+++ Assignment TwentyOne-TwentyFive/app.js
@@ -54,21 +54,60 @@
 
 
 // // Task # 07
-// document.write(`City : `)
-// let Hyderabad = (`Hyderabad`)
-// document.write(Hyderabad)
-
-// document.write(`</br>`)
-
-// var Islamabad = Hyderabad.replace("Hyder", "Islam")
-
-// document.write(`</br>`)
-
-// document.write(`After Replacement : ${Islamabad}`)
+// document.write(`City : `);
+// let Hyderabad = (`Hyderabad`);
+// document.write(Hyderabad);
+// document.write(`</br>`);
+// var Islamabad = Hyderabad.replace("Hyder", "Islam");
+// document.write(`</br>`);
+// document.write(`After Replacement : ${Islamabad}`);
 
 
 
 
+// // Task # 08
+// var message = "Ali and sami are best friends. They play Cricket and football together.";
+// document.write(message);
+// document.write("</br>" + message.replaceAll("and", ","));
 
 
 
+
+// // Task # 09
+// var value1 = `472`;
+// document.write(`Value : ${value1}`);
+// document.write(`</br>Type : String`);
+// document.write(`</br>Value : ${Number(value1)}`);
+// document.write(`</br>Type : Number`);
+
+
+
+
+// // Task # 10 
+// let userInput = prompt("Kuch bhi Likh Do bhai");
+// document.write(`User Input : ${userInput} ${"</br>"}`);
+// document.write(`In UpperCase : ${userInput.toUpperCase()}`);
+
+
+
+
+
+// // Task # 11
+// let userInput = prompt("Kuch bhi Likh Do bhai");
+// document.write(`random text that Takes from user : ${userInput}${"</br>"}`);
+// let titleCase = userInput.charAt(0).toUpperCase() + userInput.slice(1);
+// document.write(`In title Case : ${titleCase}`);
+
+
+
+
+// // Task # 12
+// let num = 67.65
+// document.write(`Number :  ${num}${"</br>"}`)
+// document.write(`String : ${num.toString().replaceAll(".", "")}`)
+
+
+
+
+
+// Task # 13
