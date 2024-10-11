1dc6966eb64981a33473fe5278e8038517d67384
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-11 11:19:37 +0500

    Assignment of Book Twent six to thirty is started

---
+++ Assignment TwentySix-Thirty/app.js
@@ -0,0 +1,20 @@
+// Math Methods
+
+
+
+// // Task # 1
+// let userNumber = +prompt("Write a Positive integer");
+// document.write(`Number : ${userNumber + "</br>"}`)
+// document.write(`Round off : ${Math.round(userNumber) + "</br>"}`)
+// document.write(`Floor Value : ${Math.floor(userNumber) + "</br>"}`)
+// document.write(`Ceil Value : ${Math.ceil(userNumber)}`)
+
+
+
+
+
+// Task # 2
+let userNumber = parseFloat(prompt("Enter a Number that you want"))
+
+
+
