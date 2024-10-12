9535d72c478aa6d232559a12fc6a1d1a0650a032
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-12 10:51:30 +0500

    Task no 03 started

--- Assignment TwentySix-Thirty/app.js
+++ Assignment TwentySix-Thirty/app.js
@@ -13,8 +13,38 @@
 
 
 
-// Task # 2
-let userNumber = parseFloat(prompt("Enter a Number that you want"))
+// // Task # 2
+// let userNumber = parseFloat(prompt("Enter a Number in negative floating point"));
+// document.write(`Number : ${userNumber + "</br>"}`);
+// document.write(`Round Off : ${Math.round(userNumber) + "</br>"}`);
+// document.write(`Floor Value : ${Math.floor(userNumber) + "</br>"}`);
+// document.write(`Ceil Value : ${Math.ceil(userNumber)}`);
+
+
+
+
+
+// // Task # 03
+// let userNumber = +prompt("Enter a number and i will absolute it...");
+// document.write(`Number : ${userNumber + "</br>"}`);
+// document.write(`Absolute Number : ${Math.abs(userNumber)}`);
+
+
+
+
+
+
+// Task # 04 
+
+
+
+
+
+
+
+
+
+
 
 
 
