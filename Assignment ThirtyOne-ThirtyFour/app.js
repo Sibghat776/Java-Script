fbe2654b87ecf4ac80c798f300cfbde0a4a8b0b2
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-14 12:42:14 +0500

    Task no 06 in process

---
+++ Assignment ThirtyOne-ThirtyFour/app.js
@@ -0,0 +1,68 @@
+// Date Methods
+
+// // Task # 01
+// document.write(`Current Date and Time : ${new Date()}`);
+
+
+
+
+// // Task # 02
+// const months = [
+//     "January", "February", "March", "April", "May", "June",
+//     "July", "August", "September", "October", "November", "December"
+// ];
+
+// const currentMonth = new Date().getMonth();
+// alert("The current month is: " + months[currentMonth]);
+
+
+
+
+
+// // Task # 03
+// let days = [`Sun` , `Mon` , `Tue` , `Wed` , `Thurs` , `Fri` , `Sat` , `Sun`]; 
+
+// let currentDay = new Date().getDay();
+// alert(`Today is : ${days[currentDay]}`);
+
+
+
+
+
+
+// // Task # 04
+// let days = new Date().getDay();
+
+// if (days == 0 || days == 6) {   
+//     alert("It's Fun day")
+// } else {
+//     alert("It's Week day")
+// }
+
+
+
+ 
+
+// // Task # 05
+// let month = new Date().getMonth()
+// if (month <= 15) {
+//     alert(`First Fifteen days of the month`)
+// } else if (month >= 16) {
+//     alert(`Last Fifteen days of the month`)
+// }
+
+
+
+
+
+
+
+// Task # 06
+document.write(`Current date : ${new Date() + "</br>"}`)
+document.write(`Elapsed MiliSeconds Sice January 1, 1970 : ${new Date("january 1, 1970").getTime() + "</br>"}`)
+document.write(`Elapsed Minutes Sice January 1, 1970 : ${new Date("january 1, 1970").getMinutes()}`)
+
+
+
+
+
