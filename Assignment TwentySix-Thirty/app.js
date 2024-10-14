605c99119ce57846ad5deee9469ed1b706b8a483
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-14 08:40:58 +0500

    Vook Assignment 26 to 30 is done...

--- Assignment TwentySix-Thirty/app.js
+++ Assignment TwentySix-Thirty/app.js
@@ -34,8 +34,56 @@
 
 
 
-// Task # 04 
+// // Task # 04 
+// document.write(`Random Number of dice show when you Refresh :  ${parseInt((Math.random() * 5) + 1)}`)
 
+
+
+
+
+
+// // Task # 05
+// let randomNumber = parseInt((Math.random() * 2) + 1);
+// document.write(`Number :  ${randomNumber + "</br>"}`);
+
+// if (randomNumber == 1) {
+//     document.write(`Random Coin Value : Tails </br>`);
+// } else if (randomNumber == 2) {
+//     document.write(`Random Coin Value : Heads`);
+// }
+
+
+
+
+
+
+// // Task # 06
+// var RandomNumber = parseInt((Math.random() * 100) + 1);
+// document.write(`Random Number Between 1 to 100 : ${RandomNumber}`);
+
+
+
+
+
+
+// // Task # 07
+// let userInput = prompt("Enter your weight in Kilograms");
+// document.write(`User weight : ${userInput}`);
+
+
+
+
+
+
+// Task # 08
+var RandomNumber = parseInt((Math.random() * 10) + 1);
+var userInput = +prompt("Enter a Number between 1 to 10")
+document.write(`Secret Number is : ${RandomNumber}`)
+if (userInput == RandomNumber) {
+    alert("Congrates your Number is match to the secret number")
+} else {
+    alert("sorry your input don't match the secret number")
+}
 
 
 
