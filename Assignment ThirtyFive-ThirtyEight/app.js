37f93ff34746e1948809eb351d1ac89e79f0a519
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-15 06:06:27 -0700

    Task 04 of Assignment 34 to 38

---
+++ Assignment ThirtyFive-ThirtyEight/app.js
@@ -0,0 +1,36 @@
+// Functions 
+
+// // Task # 01
+// function currentDate() {
+//     return new Date();
+// }
+// document.write(currentDate());
+
+
+
+
+// // task # 02
+// function greetUser() {
+//     var userFirstName = prompt("Enter your first Name?");
+//     var userLasttName = prompt("Enter your Last Name?");
+//     var userFullName = userFirstName + " " + userLasttName;
+//     document.write(`User FullName : ${userFullName}`);
+// }
+// greetUser();
+
+
+
+
+// // Task # 03
+// function addNumbers() {
+//     var num1 = +prompt("Enter a number");
+//     var num2 = +prompt("Enter a number that you want to add in first input");
+//     var result = num1 + num2;
+//     document.write(`The Input is : ${result}`)   
+// }
+// addNumbers();
+
+
+
+
+// Task # 04
