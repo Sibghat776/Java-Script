351bf5043a3231e9d394af70ce4e45d29641c93e
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-19 07:47:27 -0700

    array done

---
+++ Challenge Nine/app.js
@@ -0,0 +1,7 @@
+let students = [
+    {id: 1, name: "Shayan", score: 80, favoriteSubject: "Math"},
+    {id: 2, name: "Sami", score: 65, favoriteSubject: "English"},
+    {id: 3, name: "Sibghat", score: 92, favoriteSubject: "Islamiat"},
+    {id: 4, name: "Safi", score: 75, favoriteSubject: "Urdu"},
+    {id: 5, name: "Shahzaib", score: 95, favoriteSubject: "Sindhi"}
+]
