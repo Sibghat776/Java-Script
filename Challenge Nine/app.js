aa802084643c8654f71196b93d23371cefa5eb0b
Sibghat776 <ullahsibghat786@gmail.com>
2024-10-20 09:16:09 -0700

    Slide no 98 - 99 done...

--- Challenge Nine/app.js
+++ Challenge Nine/app.js
@@ -3,12 +3,26 @@ let students = [
     {id: 2, name: "Sami", score: 65, favoriteSubject: "English"},
     {id: 3, name: "Sibghat", score: 92, favoriteSubject: "Islamiat"},
     {id: 4, name: "Safi", score: 75, favoriteSubject: "Urdu"},
-    {id: 5, name: "Shahzaib", score: 95, favoriteSubject: "Sindhi"}
+    {id: 5, name: "Ghulam Mustafa", score: 95, favoriteSubject: "Sindhi"}
 ]
 
 let updatedStudents = students.map(function (std) {
     return {
         ...std,
-        
+        role: `Student`
     }
+});
+
+console.log(updatedStudents);
+
+let highScores = students.filter(function (std) {
+    if (std.score >= 80) return std;
 })
+
+console.log(highScores);
+
+let specificID = students.find(function (SpecificStd) {
+    if (SpecificStd.id == 3) return true;
+});
+
+console.log(specificID);
